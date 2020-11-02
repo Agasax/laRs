e2c0bd04a2bf87a2b33f575311c28eb330c4f312
Agasax <lars.saxhaug@gmail.com>
2020-11-03 00:24:37 +0100

    First function tidynamer

--- R/tidynamer.R
+++ R/tidynamer.R
@@ -1,3 +1,12 @@
+#' Change the structure of cmdstan output to fit tidybayes
+#'
+#' @param df dataframe from cmdstanr to be tidyformed
+#'
+#'
+#' @return data.frame
+#' @export
+#'
+#' @examples x <- 1
 tidynamer <- function(df) {
-  df %>% dplyr::rename_with(.cols = matches("\\.\\d"), ~ paste0(gsub(".", "[", .x, fixed = TRUE), "]"))
+  df %>% dplyr::rename_with(.cols = dplyr::matches("\\.\\d"), ~ paste0(gsub(".", "[", .x, fixed = TRUE), "]"))
 }
