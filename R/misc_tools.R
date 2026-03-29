672690d4168230574645b8f6641fc4abe1e70bc6
Lars Mølgaard Saxhaug <larsmsaxhaug@Lars-sin-MacBook-Pro.local>
2026-03-29 20:22:50 +0200

    Update with removal of carbonate dependency

--- R/misc_tools.R
+++ R/misc_tools.R
@@ -1,21 +1,6 @@
 
 
 
-#' Snip carbonate, pretty twitter code
-#' https://twitter.com/jeremy_data/status/1371171913578520587
-#' @return
-#' @export
-#'
-#'
-snip <- function() {snap <- purrr::compose(
-  ~my_code$browse(),
-  ~assign("my_code",carbonate::carbon$new(),pos=".GlobalEnv"),
-
-)
-snap()
-}
-
-
 #' Unorder ordered factor
 #'
 #' @param x Ordered factor
